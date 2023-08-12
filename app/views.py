065082897cd85c181a8522893abaa27ac396a22e
supriyapotli987 <supriyapotli987@gmail.com>
2023-08-12 13:46:33 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+def mdb5(request):
+    return render(request,'mdb5.html')
+def mdb_5(request):
+    return render(request,'mdb_5.html')
